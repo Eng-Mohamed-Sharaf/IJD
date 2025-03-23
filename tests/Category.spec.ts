7f1ad06a662b5b86c08c95d201581de64ee18df6
Eng-Mohamed-Sharaf <mohamed.sharf46@gmail.com>
2025-03-23 02:39:37 +0200

    second comit

--- tests/Category.spec.ts
+++ tests/Category.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+import { TIMEOUT } from 'dns';
 
 const languages = [
     { lang: 'ar', data: require('../locales/ar.json') },
@@ -8,15 +9,18 @@ const languages = [
 test.describe('Multilingual Category Test', () => {
     
     languages.forEach(({ lang, data }) => {
+        test.beforeEach(async ({page})=>{
+            await page.goto(`https://ijd-dashboard.vercel.app/${lang}`);
+            await page.locator('#email').fill('admin@admin.com');
+            await page.locator('#password').fill('123456');
+            await page.locator('button[type="submit"]').click();
+        });
         test(`Add and Delete Category - [${lang}]`, async ({ page }) => {
-            
-            await page.goto(`https://ijd-dashboard.vercel.app/${lang}/signin`);
+        
+            await page.goto(`https://ijd-dashboard.vercel.app/${lang}`);
 
-            await page.getByRole('textbox', { name: data.email }).fill('admin@admin.com');
-            await page.getByRole('textbox', { name: data.password }).fill('123456');
-            await page.getByRole('button', { name: data.signin }).click();
-            
             await page.getByRole('link', { name: data.Category }).click();
+
             await page.getByTestId('create-new').click();
 
             await page.getByTestId('nameAr').fill('تست');
@@ -24,13 +28,17 @@ test.describe('Multilingual Category Test', () => {
             await page.getByTestId('nameEn').fill('Test');
 
             await page.getByRole('button', { name: data.create }).click();
+            await expect(page.getByText(data.create_msg)).toBeVisible({timeout:10000});
 
-            await expect(page.getByRole('cell', { name: 'تست' }).nth(1)).toBeVisible();
+            await expect(page.getByRole('cell', { name: 'تست' }).nth(1)).toBeVisible({timeout:10000});
 
             await page.getByRole('row', { name: 'تست Test' }).getByRole('button').nth(1).click();
             await page.getByRole('button', { name: data.Delete }).click();
             
-            await expect(page.getByText(data.delete_msg)).toBeVisible();      
+            await expect(page.getByText(data.delete_msg)).toBeVisible({timeout:10000});
+  
+
+
          });
     });
 
